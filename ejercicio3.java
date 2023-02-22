c3c822d2854dd10ba6f1ef32bea6c1b26cb96b69
Aris Mufraggi Roig <122601223+ariismuf@users.noreply.github.com>
2023-02-22 08:33:37 +0100

    Add files via upload

---
+++ ejercicio3.java
@@ -0,0 +1,16 @@
+public class ejercicio3 {
+    public static void main(String[] args) {
+        System.out.println("us \t nosotros");
+        System.out.println("day \t día");
+        System.out.println("give \t dar");
+        System.out.println("these \t estos");
+        System.out.println("any \t alguno");
+        System.out.println("because \t porque");
+        System.out.println("new \t nuevo");
+        System.out.println("way \t camino");
+        System.out.println("well \t bien");
+        System.out.println("two \t dos");
+
+    }
+}
+
